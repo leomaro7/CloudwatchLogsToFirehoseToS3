185ff0dec7d012c4c21077b3fa0bba153e1bb4e7
naokis <naokis@gmail.com>
2024-08-15 16:44:14 +0900

    Initial commit

---
+++ test/cloudwatch_logs_to_firehose_to_s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudwatchLogsToFirehoseToS3 from '../lib/cloudwatch_logs_to_firehose_to_s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloudwatch_logs_to_firehose_to_s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudwatchLogsToFirehoseToS3.CloudwatchLogsToFirehoseToS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
